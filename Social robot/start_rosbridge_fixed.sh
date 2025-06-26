23e15c74d3e7e8ff987884d572dc99b4f3519e92
Hussein <118830130+hus-w03@users.noreply.github.com>
2025-06-26 14:24:32 -0700

    Update start_rosbridge_fixed.sh
    Change the roslaunch location, as it is being stored to a different place than the old code folder structure

--- Social robot/start_rosbridge_fixed.sh
+++ Social robot/start_rosbridge_fixed.sh
@@ -2,4 +2,4 @@
 source /opt/ros/noetic/setup.bash
 export ROS_HOSTNAME=localhost
 export ROS_MASTER_URI=http://localhost:11311
-roslaunch ../rosbridge_websocket_qtpc.launch
+roslaunch rosbridge_websocket_qtpc.launch
